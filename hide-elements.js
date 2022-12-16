025e13ab71f56fc20122229e37b912107eca0bee
Miika Härmä <harmamiika@gmail.com>
2022-12-16 17:51:47 +0200

    hello world sort of working

--- hide-elements.js
+++ hide-elements.js
@@ -1,42 +1,34 @@
-function hideElements(classname) {
-  console.log('yo yo yo');
-
-  const articles = document.querySelectorAll('full-article');
-  console.log(articles, 'articles');
-
-  var elements = document.querySelectorAll(classname);
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].style.display = 'none';
-  }
-}
 console.log('yo');
 
-console.log('yo2');
-console.log('yop3');
+// add hello world to the body
+document.body.appendChild(document.createTextNode('Hello World!'));
 
-chrome.browserAction.onClicked.addListener(function (tab) {
-  console.log('yo2');
-  chrome.tabs.executeScript({
-    // code: 'hideElements(".classname");',
-    code: 'console.log("Hello, World!");',
-  });
-});
+const hiddenElements = document.querySelectorAll('.hidden');
+console.log(hiddenElements, 'hidden elements initial');
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('yo man weed');
-  const hiddenElements = document.querySelectorAll('.hidden');
-  hiddenElements.forEach(function (element) {
-    element.style.display = 'none';
-  });
-
-  // add app bar to page
-
-  const appbar = document.createElement('div');
-  appbar.classList.add('appbar');
-  appbar.innerHTML = `
-    <div class="appbar__item">
-      <button class="appbar__button">Hide</button>
-    </div>  
-  `;
-  document.body.appendChild(appbar);
+  console.log('Page loaded!');
 });
+
+document.addEventListener('scroll', function () {
+  console.log('scrolling');
+  const hiddenElements = document.querySelectorAll('.full-article');
+  // select items with classname full-article
+
+  // log document body contents
+  console.log(document.body);
+
+  console.log(hiddenElements);
+});
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   console.log('Page loaded!');
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   console.log('executing');
+//   const hiddenElements = document.querySelectorAll('.hidden');
+//   hiddenElements.forEach(function (element) {
+//     element.style.display = 'none';
+//   });
+// });

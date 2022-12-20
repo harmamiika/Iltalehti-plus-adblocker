ce6da27615009dc64a5f3b4967d372b37c157bb3
Miika Härmä <harmamiika@gmail.com>
2022-12-20 21:34:23 +0200

    hidesticker, cleanup

--- hide-elements.js
+++ hide-elements.js
@@ -1,11 +1,14 @@
 // add hello world to the body
 // artles hidden text
 
+const hide = (element) => {
+  element.style.visibility = 'hidden';
+  element.style.height = '0px';
+  element.style.paddingBottom = '0px';
+};
+
 const hideArticles = () => {
-  // if no articler with classname full-article or half-article and with svg with classname il-plus-svg-logo inlin
-
   const articles = document.querySelectorAll('.full-article, .half-article');
-
   articles.forEach((article) => {
     if (article.querySelector('.il-plus-svg-logo.inline')) {
       article.style.visibility = 'hidden';
@@ -15,15 +18,9 @@ const hideArticles = () => {
 
 const hideContainers = () => {
   const containers = document.querySelectorAll('.card.fp-container');
-  const containers2 = document.querySelectorAll('.fp-container');
-  const containers3 = document.querySelectorAll('.card');
-  console.log(containers, 'containers');
-  console.log(containers2, 'containers2');
-  console.log(containers3, 'containers3');
   containers.forEach((container) => {
-    // if containers child elements with classname full-article or half-article and with svg with classname il-plus-svg-logo inline all have visibility hidden
-    // hide container
     const articles = container.querySelectorAll('.full-article, .half-article');
+
     let hidden = true;
     articles.forEach((article) => {
       if (article.style.visibility !== 'hidden') {
@@ -32,20 +29,18 @@ const hideContainers = () => {
     });
 
     if (hidden) {
-      console.log('hid something');
-      container.style.visibility = 'hidden';
-      container.style.height = '0px';
+      hide(container);
     }
   });
 };
 
-// const hideNewStick = () => {
-//   const newStick = document.querySelector('.newsticker-container');
-//   newStick.style.visibility = 'hidden';
-// };
+const hideSticker = () => {
+  const newStick = document.querySelector('.newsticker-container');
+  hide(newStick);
+};
 
 document.addEventListener('scroll', function () {
-  // hideNewStick();
+  hideSticker();
   hideArticles();
   hideContainers();
   // select items with classname full-article
